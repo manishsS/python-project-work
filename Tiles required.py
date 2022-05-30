f72fa2b1b7895702dc8792c1bcfb281c78f77b67
manishsS <105847386+manishsS@users.noreply.github.com>
2022-05-30 20:42:37 +0530

    Create Tiles required.py

---
+++ Tiles required.py
@@ -0,0 +1,8 @@
+lt= float(input("Enter Length of Tile :"))
+bt= float(input("Enter Breadth of Tile :"))
+art= lt*bt
+fl=int(input("Enter Length of Floor : "))
+fb=int(input("Enter Beadth of Floor : "))
+arf=fl*fb
+nt=arf/art
+print(f" Number of Tiles Required are {nt}")
